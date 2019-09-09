aadb6ed71479602a95cc8bf268996e1c735c027c
Emmanuel Pilande <epilande@gmail.com>
2019-09-08 21:21:09 -0700

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<div align="center">
+  <h1>epilande 📟</h1>
+</div>
+
+## Usage
+
+```bash
+$ npx epilande
+```
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, Box, Text, Color } from "ink";
+
+render(
+  <Box flexDirection="column">
+    <Box marginBottom={1}>
+      <Text>
+        Hello World! <Color green>epilande</Color>
+      </Text>
+    </Box>
+  </Box>,
+);
